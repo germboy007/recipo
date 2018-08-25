58e58005020f5a82e687f9bbcdfc3bd8fce77072
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-08-25 23:44:30 +0530

    recipe view

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import {Search} from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 // Global State of the App
@@ -45,13 +46,16 @@ const controlRecipe = async () => {
   const id = window.location.hash.replace('#', '');
 
   if(id){
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
     state.recipe = new Recipe(id);
     try {
       await state.recipe.getRecipe();
       state.recipe.parseIngredients();
       state.recipe.calcTime();
       state.recipe.calcServings();
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
     } catch (err) {
       alert('Error processing recipe!');
     }
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -34,6 +34,7 @@ export default class Recipe {
   parseIngredients() {
     const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
     const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+    const units = [...unitsShort, 'kg', 'g'];
     const newIngredients = this.ingredients.map(el => {
       let ingredients = el.toLowerCase();
       unitsLong.forEach((unit, i)=> {
@@ -41,7 +42,7 @@ export default class Recipe {
       });
       ingredients = ingredients.replace(/ *\([^)]*\) */g, ' ');
       const arrIng = ingredients.split(' ');
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
       let objIng;
 
       if(unitIndex > -1 ) {
