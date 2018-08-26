a4a804a53920dc133f5ad3dc74bb9bd32f686a35
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-08-26 11:49:16 +0530

    updated recipe view for servings

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,5 @@
-const key = 'd3789abbeef3d248cdcc97cd023b7f1d';
+//const key = 'd3789abbeef3d248cdcc97cd023b7f1d';
+const key = 'f18f42ec336a2a28be79b04768832cc6';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 
 export {key, proxy};
